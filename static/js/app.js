540583cdcfcfad172c831933e426f563f739a33d
Shimsy Varkey <shimsyanna@gmail.com>
2020-11-09 22:49:51 -0600

    Created a function to display UFO sightings

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,38 @@
+// from data.js
+var tableData = data;
+
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // print the data from data.js
-console.log(data);
+// console.log(data);
 
 // loop through data and log each object
-data.forEach(ufoRecord => {
-    console.log(ufoRecord);
+// Create a function to display UFO sightings
+function tableDisplay(data) {
+    data.forEach(ufoRecord => {
+        console.log(ufoRecord);
 
-    // Use d3 to append one table row `tr` for each ufo sighting report object
-    var row = tbody.append("tr");
+        // Use d3 to append one table row `tr` for each ufo sighting report object
+        var row = tbody.append("tr");
 
-    // use object entries to log each ufo report value
-    Object.entries(ufoRecord).forEach(([key, value]) => {
-        console.log(key, value);
+        // use object entries to log each ufo report value
+        Object.entries(ufoRecord).forEach(([key, value]) => {
+            console.log(key, value);
 
-        // Use d3 to append 1 cell per ufo report value 
-        var cell = row.append("td");
+            // Use d3 to append 1 cell per ufo report value 
+            var cell = row.append("td");
 
-        // Use d3 to update each cell's text with ufo report values 
-        cell.text(value);
+            // Use d3 to update each cell's text with ufo report values 
+            cell.text(value);
+
+        })
 
     })
 
-})
+}
 
+tableDisplay(data)
 
 
 
